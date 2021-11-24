09b2c578b8413827357dfc88c545b6e3734bfe56
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2021-11-24 07:59:32 -0800

    added

--- JS/exemples-veille/if-statement.js
+++ JS/exemples-veille/if-statement.js
@@ -1,9 +1,10 @@
-var x = window.prompt(" guess number superior of 4 ");
 
-if (x=4) {
+var y = window.prompt(" guess number superior of 4 ");
+
+if (y=5) {
     alert("the wrong number !!")    
 }
- else if(x=5) {
+ else if(y=6) {
     alert("the wrong number again !!")
     
 }
