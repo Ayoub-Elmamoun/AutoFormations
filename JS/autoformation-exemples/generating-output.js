147cd3e501c9cbf131513eeb5d1d95e421c9920f
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2021-11-25 02:56:37 -0800

    added

--- JS/autoformation-exemples/generating-output.js
+++
@@ -1,4 +0,0 @@
-// OUTPUT IN CONSOLE 
-
-    //PRINT A SIMPLE TEXT
-    console.log("hello brothers")
--- JS/autoformation-exemples/variable.js
+++
@@ -1,5 +0,0 @@
-
-         var x = "hello";
-         var y = "world";
-         var z = x+y;
-         alert(z);
